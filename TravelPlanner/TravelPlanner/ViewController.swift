cbb09c6d4f39a3599457f5673f680d8614f506a5
Francois Combe <francois.combe@lunabee.com>
2019-06-24 19:51:43 +0200

    init xcode project

---
+++ TravelPlanner/TravelPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelPlanner
+//
+//  Created by François Combe on 24/06/2019.
+//  Copyright © 2019 francois. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
