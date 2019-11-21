b47fa3cbe29cb260b3f7f1de5c94dcf2d1525b2c
François Combe <francois.combe@gmx.fr>
2019-11-21 21:47:37 +0100

    no message

---
+++ TravelPlanner/TravelPlanner/Modules/APIModel/RTrip.swift
@@ -0,0 +1,14 @@
+//
+//  RTrip.swift
+//  TravelPlanner
+//
+//  Created by François Combe on 21/11/2019.
+//  Copyright © 2019 francois. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RTrip: RObject {
+    <#code#>
+}
